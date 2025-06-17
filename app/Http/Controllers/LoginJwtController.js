14cc1f6f888f2d4649c26c98e1ddab695ecea668
Diego0Alves <anibodyelse1@gmail.com>
2025-06-17 20:00:34 -0300

    LoginJwt

---
+++ app/Http/Controllers/LoginJwtController.js
@@ -0,0 +1,52 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import professoresModel from '../../Models/professoresModel';
+import { request } from 'express';
+
+export default async (request, response) => {
+
+    const email = request.body.email;
+    const senha = request.body.senha;
+
+    const JWT_SECRET = process.env.JWT_SECRET;
+    const JWT_EXPIRES = '10m';
+
+    try {
+        const user = await professoresModel.findOne({
+            where: {
+                email: email
+            },
+            include: [
+                "role"
+            ]
+        });
+        if (!user) {
+            return response.status(401).json({ message: 'Usuário Invalido' });
+        }
+
+        const senhaValida = await bcrypt.compare(senha, user.senha);
+        
+        if (!senhaValida) {
+            return response.status(401).json({ message: 'Senha Invalida' });
+        }
+
+        const payload = {
+            id: user.id,
+            email: user.email,
+            nome: user.nome,
+            role: user.role.nome
+        };
+
+        const token = jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES });
+        
+        return response.json({
+            token: token,
+            expiresin: JWT_EXPIRES,
+
+        });
+    } catch (error) {
+        console.error('Erro ao autenticar usuário:', error);
+        return response.status(500).json({ message: 'Erro interno do servidor' });
+    }
+
+}

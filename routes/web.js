d7e0603372f6dd2a4d1f1c6edeb8c1c3dab797f3
Diego0Alves <anibodyelse1@gmail.com>
2025-06-30 19:43:25 -0300

    up

--- routes/web.js
+++ routes/web.js
@@ -4,23 +4,16 @@ import path from 'path';
 import swaggerUi from 'swagger-ui-express';
 
 import DateController from '../app/Http/Controllers/DateController.js';
-//import ListPublicFilesController from '../app/Http/Controllers/ListPublicFilesController.js';
 //import swaggerGenerate from '../Core/SwaggerCore/swaggerGenerate.js';
 
 export default (function () {
 
     const router = Router();
 
-    /** Servir o public estaticamente, tanto para arquivos como para os assets de frontend */
-    // NÃO SERÁ CHAMADO CASO TENHA A CAMADA DE NGINX COM ARQUIVOS ESTÁTICOS
     router.use(express.static(path.join(CONSTANTS.DIR, 'public')));
 
-    // Rota para listar arquivos na pasta 'public'
-    // NÃO SERÁ CHAMADO CASO TENHA A CAMADA DE NGINX COM ARQUIVOS ESTÁTICOS
-    //router.get('/', ListPublicFilesController);
-
-    // Documentação Swagger
-    //router.use('/docs', swaggerUi.serve, swaggerGenerate);
+    // Swagger
+    //router.use('/swagger', swaggerUi.serve, swaggerGenerate);
 
     router.get('/date', DateController);
 
